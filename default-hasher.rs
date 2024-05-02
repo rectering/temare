552640a36eeeaa8252faf7d9388cacb7a36410cf
Agustin Puifil <124407245+rectering@users.noreply.github.com>
2024-05-02 18:56:28 -0300

    day 2

---
+++ default-hasher.rs
@@ -0,0 +1,37 @@
+use std::hash::{DefaultHasher, Hash, Hasher};
+
+struct Person {
+    id: u32,
+    name: String,
+    phone: u64,
+}
+
+impl Hash for Person {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.id.hash(state);
+        self.phone.hash(state);
+    }
+}
+
+fn calculate_hash<T: Hash>(t: &T) -> u64 {
+    let mut s = DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
+}
+
+fn main() {
+    let person1 = Person {
+        id: 5,
+        name: "Janet".to_string(),
+        phone: 555_666_7777,
+    };
+
+    let person2 = Person {
+        id: 5,
+        name: "Bob".to_string(),
+        phone: 555_666_7777,
+    };
+
+    assert!(calculate_hash(&person1) != calculate_hash(&person2));
+
+}
